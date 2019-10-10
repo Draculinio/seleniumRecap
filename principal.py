7d00459b363579a8b9056ec7fe58a2dc4010a7eb
Draculinio <stackpointerex@gmail.com>
2019-10-10 09:04:41 -0400

    Capitulo 4

--- principal.py
+++ principal.py
@@ -1,23 +1,30 @@
 from selenium import webdriver
 import unittest
 import time
-
+from pages.pageindex import *
 
 class Items(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome('chromedriver.exe')
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--incognito')
+        #chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome('chromedriver.exe', options=chrome_options)
         self.driver.get('http://automationpractice.com/index.php')
 
     #def test_do_nothing(self):
     #    pass
 
     def test_view_item_page(self):
-        self.driver.find_element_by_id('search_query_top').send_keys('dress')
-        self.driver.find_element_by_name('submit_search').click()
+        page_index = Page_index(self.driver)
+        page_index.search_items('dress')
         self.driver.implicitly_wait(5)
         self.driver.find_element_by_xpath('//*[@id="center_column"]/ul/li[1]/div/div[1]/div/a[1]/img').click()
         title = self.driver.find_element_by_xpath('//h1[@itemprop="name"]').text
         self.assertEqual(title, 'Printed Summer Dress', 'Text should be different')
+
+    def test_search_with_no_items(self):
+        page_index = Page_index(self.driver)
+        page_index.search_items('computer')
 
     def tearDown(self):
         self.driver.quit()
